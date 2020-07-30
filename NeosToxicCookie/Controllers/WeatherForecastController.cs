3a177b60cdd51a301ae014d6dfabc1d1f1c50132
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2020-07-29 23:53:39 -0700

    Add project files.

---
+++ NeosToxicCookie/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NeosToxicCookie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            Random rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+            })
+            .ToArray();
+        }
+    }
+}
